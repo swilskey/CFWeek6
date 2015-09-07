6947abf6d638d433f527fdaad5e18cd88568282c
swilskey <swilskey41@gmail.com>
2015-09-07 13:21:53 -0700

    New Project

---
+++ Hotel Booker/Hotel Booker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hotel Booker
+//
+//  Created by Sam Wilskey on 9/7/15.
+//  Copyright (c) 2015 Wilskey Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hotel Booker/Hotel Booker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hotel Booker
+//
+//  Created by Sam Wilskey on 9/7/15.
+//  Copyright (c) 2015 Wilskey Labs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
