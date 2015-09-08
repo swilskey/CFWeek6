d3d50a8bfc66dbac09994d5d7f5d58aa4efaf8c8
swilskey <swilskey41@gmail.com>
2015-09-07 18:51:06 -0700

    Added TableView

--- Hotel Booker/Hotel Booker/ViewController.m
+++ Hotel Booker/Hotel Booker/ViewController.m
@@ -8,11 +8,22 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @end
 
 @implementation ViewController
+
+- (void)loadView {
+  UIView *rootView = [[UIView alloc] init];
+  rootView.backgroundColor = [UIColor whiteColor];
+  
+  UITableView *hotelTableView = [[UITableView alloc] init];
+  
+  [rootView addSubview:hotelTableView];
+  
+  self.view = rootView;
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
@@ -22,6 +33,32 @@
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
+}
+
+#pragma Mark - Navigation
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+  if ([segue.identifier isEqualToString:@"ShowRooms"]) {
+    
+  }
+}
+
+#pragma Mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell = (UITableViewCell *) [tableView dequeueReusableCellWithIdentifier:@"HotelCell" forIndexPath:indexPath];
+  
+  return cell;
+}
+
+#pragma Mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  [self performSegueWithIdentifier:@"ShowRooms" sender:self];
 }
 
 @end
